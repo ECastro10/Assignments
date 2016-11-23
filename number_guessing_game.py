f53ee13965b6b1c3ad00cae000159aa868333ef8
Emanuel Castro <eecastro1011@gmail.com>
2016-11-22 20:51:13 -0800

    You have ten tries to guess my number 1-20

---
+++ number_guessing_game.py
@@ -0,0 +1,21 @@
+import random
+'''
+user_num = int(input("type in a number > "))
+while user_num < 23:
+    user_num = int(input("Enter in a bigger number > "))
+print("exit")
+'''
+com_num = random.randint(1,20)
+is_win = False
+user_num = int(input("Guess what number I am thinking of, you have 10 tries.\n\
+Hint: It might be 1 - 20 > "))
+tries = 10
+print(com_num)
+while user_num != com_num:
+    tries -= 1
+    print("You have {} guesses left!".format(tries))
+    if tries == 0:
+        print("You lose!")
+        break
+    user_num = int(input("Incorrect! Guess again > "))
+print("My number was {}".format(com_num))
