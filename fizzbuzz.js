1e26499976c04aeffa00f9cb35098fbc05a414b2
Emanuel Castro <eecastro1011@gmail.com>
2016-12-27 18:59:31 -0800

    fizzbuzz done on javascript

---
+++ fizzbuzz.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Lexi on 12/27/2016.
+ */
+function fizzbuzz() {
+    for (var i=1; i<=100; i++)
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log('fizzbuzz');
+    }
+        else if (i % 3 == 0) {
+            console.log('fizz');
+    }
+        else if (i % 5 == 0) {
+            console.log('buzz');
+    }
+        else {
+            console.log(var i);
+    }
+
+
+
+
+fizzbuzz()
